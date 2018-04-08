f34b0e91c0e65e7d7f905a0fc09b839a39bc4a4c
AnastasiaPodorogina <drrobertt7@gmail.com>
2018-04-08 19:13:30 +0300

    VirtualPonyBot demo

--- JsonFromRequest.py
+++ JsonFromRequest.py
@@ -29,13 +29,27 @@ class Ticket:
         self.airline = airline
         self.price = price
         self.transfers = transfers
-        self.weight = 0
+        self.transf_coeff = 1 - transfers * 0.5
+        self.price_coeff = 0.
+        self.weight = self.transf_coeff
 
     def ticket_list(self):
         return [self.curr, self.airline, self.price, self.transfers]
 
     def calculate_weight(self, value):
         self.weight += value
+
+    def get_price(self):
+        return self.price
+
+    def set_price_coeff(self, min_price):
+        self.price_coeff = min_price / self.price
+
+    def get_price_coeff(self):
+        return self.price_coeff
+
+    def get_airline(self):
+        return self.airline
 
 
 def ticket_list(js):
@@ -48,7 +62,7 @@ def ticket_list(js):
 
 def get_json_port():
     json_port = json.load(open("aviaport.json"))
-    print(json.dumps(json_port, indent=4, sort_keys=True))
+    # print(json.dumps(json_port, indent=4, sort_keys=True))
     return json_port
 
 
@@ -57,15 +71,48 @@ def get_json_city():
     js_c = json.dumps(json_city.json(), indent=4, sort_keys=True)
     city_dict = dict()
     for d in json_city.json():
-        city_dict[d["code"]] = d["name_translations"]["ru"]
-    return json_city.json()
+        city_dict[d["name_translations"]["ru"]] = d["code"]
+    return city_dict
 
 
-if __name__ == "__main__":
-    js = JsonFromRequest("2018-04", "MOW", "BCN")
-    get_json_city()
-    get_json_port()
+def min_price(*args):
+    price_list = list()
+    for arg in args:
+        price_list.append(arg.get_price())
+    price_list.sort()
+    return price_list[0]
 
-# print(array_tk[0])
-# tk = Ticket(js.get_json()["currency"], array_tk[0])
-# print(json.dumps(js.get_json()["data"], indent=4, sort_keys=True))
+def YOBANI_VROT(test_dict):
+    js_cities = get_json_city()
+
+    js = JsonFromRequest("2018-04", js_cities[test_dict["origin"]], js_cities[test_dict["destination"]])
+    t_list = ticket_list(js)
+    print(t_list)
+    price_list = list()
+    for arg in t_list:
+        price_list.append(arg.get_price())
+    price_list.sort()
+    min = price_list[0]
+
+    for tk in t_list:
+        tk.set_price_coeff(int(min))
+
+    coeffs = get_json_port()
+
+    for arg in t_list:
+        if arg.get_airline() in coeffs:
+            d = coeffs[arg.get_airline()]
+            arg.calculate_weight(d["bort_feed"]*0.01*int(test_dict["bort_feed"]))
+            arg.calculate_weight(d['hand_clad'] * 0.01 * int(test_dict['hand_clad']))
+            arg.calculate_weight(d['p_space'] * 0.01 * int(test_dict['p_space']))
+            arg.calculate_weight(d["entertain"] * 0.01 * int(test_dict["entertain"]))
+            arg.calculate_weight(d["clean"] * 0.01 * int(test_dict["clean"]))
+            arg.calculate_weight(d["sells"] * 0.01 * int(test_dict["sells"]))
+            arg.calculate_weight(d["staff_work"] * 0.01 * int(test_dict["staff_work"]))
+            arg.calculate_weight(arg.get_price_coeff())
+        else:
+            arg.calculate_weight(0.5)
+            arg.calculate_weight(arg.get_price_coeff())
+
+    return t_list
+
---
+++ ponyBot.py
@@ -0,0 +1,98 @@
+import telebot
+from telebot import types
+import JsonFromRequest as FUCK
+
+TOKEN = "583902166:AAHRknj8tijEEt82RL8X3IGZs4lgV16RjiE"
+TXT_HELLO = "Привет! Я Пони - твой друг)\nСегодня я помогу тебе выбрать самые классные билеты, чтобы ты смог отправиться в путешествие ;)\n\n\nИз какого ты города?"
+CHAT_ID = 187022130
+tb = telebot.TeleBot(TOKEN)
+
+user = tb.get_me()
+
+answer_dict = {"origin": "", "destination": "", "d_date": "", "bort_feed": "", "hand_clad": "", "p_space": "",
+               "entertain": "", "clean": "", "sells": "", "staff_work": ""}
+
+count = 0
+
+
+def deadline1(message, question, key):
+    tb.reply_to(message, question, reply_markup=markup)
+    answer_dict[key] = message.text
+    print(answer_dict)
+
+
+def deadline(message, question, key):
+    tb.reply_to(message, question)
+    answer_dict[key] = message.text
+    print(answer_dict)
+
+
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('10')
+itembtnv = types.KeyboardButton('20')
+itembtnc = types.KeyboardButton('30')
+itembtnd = types.KeyboardButton('40')
+itembtne = types.KeyboardButton('50')
+itembtnaa = types.KeyboardButton('60')
+itembtnvv = types.KeyboardButton('70')
+itembtncc = types.KeyboardButton('80')
+itembtndd = types.KeyboardButton('90')
+itembtnee = types.KeyboardButton('100')
+markup.row(itembtna, itembtnv, itembtnc, itembtnd, itembtne)
+markup.row(itembtnaa, itembtnvv, itembtncc, itembtndd, itembtnee)
+
+
+@tb.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    global count
+    count = 0
+    tb.reply_to(message, TXT_HELLO)
+    count += 1
+
+
+@tb.message_handler(func=lambda message: True)
+def msg_hd(message):
+    global count
+    if count == 1:
+        deadline(message, "Куда ты хочешь полететь?)", "origin")
+
+    if count == 2:
+        deadline(message, "Когда ты хочешь полететь в {}?)".format(message.text), "destination")
+
+    if count == 3:
+        deadline1(message, "На сколько вы любите вкусно поесть на борту?)", "d_date")
+
+    if count == 4:
+        deadline1(message, "Много ли вы планируете взять с собой в самолет?", "bort_feed")
+
+    if count == 5:
+        deadline1(message, "Насколько вы интроверт?", "hand_clad")
+
+    if count == 6:
+        deadline1(message, "Как вы относитесь к развлечениям на борту?", "p_space")
+
+    if count == 7:
+        deadline1(message, "На сколько вы брезгливый человек?", "entertain")
+
+    if count == 8:
+        deadline1(message, "Хотели бы вы что-нибудь купить во время полета?", "clean")
+
+    if count == 9:
+        deadline1(message, "На сколько вам важен сервис?", "sells")
+
+    if count == 10:
+        deadline1(message, "Спасибо за ответики ;)", "staff_work")
+        tb.send_message(CHAT_ID, "Подождите, сейчас поняша подберет билетик...;)")
+        max_tiket = FUCK.YOBANI_VROT(answer_dict)[0]
+        for ticket in FUCK.YOBANI_VROT(answer_dict):
+            if ticket.weight > max_tiket.weight:
+                max_tiket = ticket
+
+        msg = "Билет авиакомпании {}\nЦена:{}\nКоличество пересадок:{}\n".format(max_tiket.ticket_list()[1], max_tiket.ticket_list()[2], max_tiket.ticket_list()[3])
+        tb.send_message(CHAT_ID, msg)
+        tb.send_message(CHAT_ID, "Ваш билетик;)")
+
+    count += 1
+
+
+tb.polling(none_stop=False, interval=3, timeout=20)
